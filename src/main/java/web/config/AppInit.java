68abc80835aa251392b5c1bea79a935d597a7a36
BLM <Lifmon34rus@gmail.com>
2023-03-03 09:30:29 +0300

    Commit

---
+++ src/main/java/web/config/AppInit.java
@@ -0,0 +1,33 @@
+package web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    // Метод, указывающий на класс конфигурации
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{
+                AppConfig.class
+        };
+    }
+
+
+    // Добавление конфигурации, в которой инициализируем ViewResolver, для корректного отображения html.
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{
+                WebConfig.class
+        };
+    }
+
+
+    /* Данный метод указывает url, на котором будет базироваться приложение */
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
---
+++ src/main/java/web/models/User.java
@@ -0,0 +1,55 @@
+package web.models;
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Entity
+@Component
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public User() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
